f08a6f5acde8ede1a42293c36b420cc06fd19bd8
Deltron18 <karans0518@gmail.com>
2026-05-02 22:22:02 +0530

    index.js

--- LMS/frontend/src/App.jsx
+++ LMS/frontend/src/App.jsx
@@ -26,7 +26,7 @@ import ViewLecture from './pages/ViewLecture'
 import SearchWithAi from './pages/SearchWithAi'
 import getAllReviews from './customHooks/getAllReviews'
 
-export const serverUrl = "https://ai-lms-backend-2el4.onrender.com/"
+export const serverUrl = "https://ai-lms-backend-2el4.onrender.com"
 
 function App() {
   
